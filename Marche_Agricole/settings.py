7d05b8f6f78a7999a6a489e6053c5ffa78db76dd
ikhalilthiam18-dev <i.khalil.thiam18@gmail.com>
2026-03-16 18:56:38 +0000

    Préparation déploiement PythonAnywhere

--- Marche_Agricole/settings.py
+++ Marche_Agricole/settings.py
@@ -3,8 +3,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-moi-plus-tard'
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['Khalil18.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -75,8 +75,12 @@ TIME_ZONE = 'Africa/Dakar'
 USE_I18N = True
 USE_TZ = True
 
+
+
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- Marche_Agricole/wsgi.py
+++ Marche_Agricole/wsgi.py
@@ -1,5 +1,11 @@
-
 import os
+import sys
+
+path = '/home/Khalil18/Marche_Agricole/Marche_Agricole'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Marche_Agricole.settings'
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','Marche_Agricole.settings')
-application=get_wsgi_application()
+application = get_wsgi_application()
