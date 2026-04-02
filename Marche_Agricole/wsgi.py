811c17ce9dd29d2ed994b3705d6e3b8bc5330060
ikhalilthiam18-dev <i.khalil.thiam18@gmail.com>
2026-04-02 16:35:29 +0000

    fix django allowed_hosts for vercel

--- Marche_Agricole/wsgi.py
+++ Marche_Agricole/wsgi.py
@@ -1,10 +1,7 @@
 import os
-import sys
-
-path = '/home/Khalil18/Marche_Agricole/Marche_Agricole'
-if path not in sys.path:
-    sys.path.append(path)
-
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','Marche_Agricole.settings')
-application=get_wsgi_application()
+
+# Assure-toi que le nom correspond bien au dossier de ton projet
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Marche_Agricole.settings')
+
+application = get_wsgi_application()
