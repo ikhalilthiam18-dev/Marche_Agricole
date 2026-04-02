0d700e75c6d45d8cda27e3021bfb258ad77eda1a
ikhalilthiam18-dev <i.khalil.thiam18@gmail.com>
2026-04-02 16:20:21 +0000

    fix django allowed_hosts for vercel

--- Marche_Agricole/settings.py
+++ Marche_Agricole/settings.py
@@ -11,7 +11,7 @@ DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() == "true"
 # Autoriser localhost + domaine principal + tous les sous-domaines vercel.app
 ALLOWED_HOSTS = os.getenv(
     "DJANGO_ALLOWED_HOSTS",
-    "127.0.0.1,localhost,marche-agricole.vercel.app,.vercel.app"
+    "127.0.0.1,localhost,.vercel.app"
 ).split(",")
 
 INSTALLED_APPS = [
